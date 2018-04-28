f7b00fd60b573b69ec858d972e402535604df10a
RahulBilimoria <rahulbilimoria1@gmail.com>
2018-04-28 16:30:37 -0400

    single page app works

---
+++ controller.js
@@ -0,0 +1,14 @@
+angular.module("projects", []).controller("projectsController",
+['$scope', '$http',
+
+function($scope, $http){
+    $scope.show = 'all';
+    
+    $http.get('projects.json').then(function(data) {
+        $scope.projects = data.data;
+    })
+    
+    $scope.showList = function(value){
+        $scope.show = value;
+    }
+}])
