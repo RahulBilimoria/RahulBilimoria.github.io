9f0c703de779825c0aeaf11724da06ddb29a4ba0
RahulBilimoria <rahulbilimoria1@gmail.com>
2018-04-30 19:25:53 -0400

    added pictures

--- controller.js
+++ controller.js
@@ -2,13 +2,13 @@ angular.module("projects", []).controller("projectsController",
 ['$scope', '$http',
 
 function($scope, $http){
-    $scope.show = 'all';
-    
-    $http.get('projects.json').then(function(data) {
-        $scope.projects = data.data;
-    })
-    
-    $scope.showList = function(value){
-        $scope.show = value;
-    }
+  $scope.show = 'all';
+
+  $http.get('projects.json').then(function(data) {
+      $scope.projects = data.data;
+  })
+
+  $scope.showList = function(value){
+      $scope.show = value;
+  }
 }])
