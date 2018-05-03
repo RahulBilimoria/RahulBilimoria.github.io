33f01f1ecd5176fc17a1c1bf5df88a253d06b065
RahulBilimoria <rahulbilimoria1@gmail.com>
2018-05-03 19:00:43 -0400

    added more info for projects

--- controller.js
+++ controller.js
@@ -2,8 +2,9 @@ angular.module("projects", []).controller("projectsController",
 ['$scope', '$http',
 
 function($scope, $http){
-  $scope.show = 'all';
   $scope.showProject = false;
+  $scope.show = 'all';
+  $scope.projectInfo = null;
 
   $http.get('projects.json').then(function(data) {
       $scope.projects = data.data;
@@ -13,7 +14,8 @@ function($scope, $http){
       $scope.show = value;
   }
   
-  $scope.setProject = function(value){
+  $scope.setProject = function(value, id){
+    $scope.projectInfo = $scope.projects[id];
     $scope.showProject = value;
   }
 }])
