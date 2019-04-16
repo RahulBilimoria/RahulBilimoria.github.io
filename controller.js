27255263e9c2b3561f2dfb09d9afbd39192335b4
RahulBilimoria <rahulbilimoria1@gmail.com>
2019-04-16 14:39:16 -0400

    fixed bugs in threejs for mobile and refreshing a project page

--- controller.js
+++ controller.js
@@ -3,40 +3,56 @@ var app = angular.module("projects", ["ngRoute", "ngAnimate", "ui.bootstrap"]);
 app.config(function($routeProvider) {
   $routeProvider
     .when("/", {
-      templateUrl: "projects2.html",
-  })
+      templateUrl: "projects2.html"
+    })
+    .when("/flappyjump", {
+      templateUrl: "html/flappyjump.html"
+      /* use :projectName instead */
+    })
     .otherwise({
-      templateUrl: "project.html",
-  })
-})
+      templateUrl: "project.html"
+    });
+});
 
-app.controller("projectsController",
-['$scope', '$http',
+app.controller("projectsController", [
+  "$scope",
+  "$http",
 
-  function($scope, $http){
-    $scope.show = 'all';
+  function($scope, $http, $location) {
+    $scope.show = "all";
     $scope.projectInfo = null;
     $scope.active = 0;
-    
-    $http.get('projects.json').then(function(data) {
-        $scope.projects = data.data;
-    })
 
-    $scope.showList = function(value){
-        $scope.show = value;
-    }
+    $http.get("projects.json").then(function(data) {
+      $scope.projects = data.data;
+      var url = window.location.href.split("#!/")[1];
+      if (!url) return;
+      for (var i = 0; i < $scope.projects.length; i++) {
+        if ($scope.projects[i].link.indexOf(url) > 0) {
+          $scope.projectInfo = $scope.projects[i];
+          $scope.slideshow = $scope.projectInfo.project.slideshow;
+          window.scrollTo(0, 0);
+          break;
+        }
+      }
+    });
 
-    $scope.setProject = function(value, id){
+    $scope.loadProject = function(id) {
       $scope.projectInfo = $scope.projects[id];
       $scope.slideshow = $scope.projectInfo.project.slideshow;
-      window.scrollTo(0,0);
-    }
+      window.scrollTo(0, 0);
+    };
 
-    $scope.getIcon = function(value){
+    $scope.showList = function(value) {
+      $scope.show = value;
+    };
+
+    $scope.getIcon = function(value) {
       if (value === "C#") value = "CSharp";
       if (value === "HTC Vive") value = "htcvive";
       if (value === "Open Weather Map API") value = "openweathermap";
       if (value === "Visual Studio") value = "visualstudio";
       return "./icon/" + value + ".png";
-    }
-}])
+    };
+  }
+]);
